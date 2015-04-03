4be821c9222f4f612eaf486f17317a22f3ee29eb
grant nicholas <grantnicholas2015@u.northwestern.edu>
2015-04-03 13:25:38 -0500

    fixed modules require.js

--- assets/js/app/.module-cache/c03222c63b2785339358f16bd34e2510fc4f2fba.js
+++ assets/js/app/.module-cache/c03222c63b2785339358f16bd34e2510fc4f2fba.js
@@ -1,29 +1,30 @@
   $(function() {
+      console.log('in searchbar.js');
 
-      var Post = React.createClass({
+      var Post = React.createClass({displayName: "Post",
 
       render: function() {
 
           return (
 
-            <li className={this.props.is_selected}> 
-              <a href= {this.props.url}>
-              <div className = "row">
-                <div className="large-3 columns">
-                    <img className={this.props.img ===undefined ? 'hide' : ''} src={this.props.img}/>
-                </div>
-                <div className="large-9 columns">
-                  {this.props.title}
-                </div>
-              </div>
-              </a>
-            </li>
+            React.createElement("li", {className: this.props.is_selected}, 
+              React.createElement("a", {href: this.props.url}, 
+              React.createElement("div", {className: "row"}, 
+                React.createElement("div", {className: "large-3 columns"}, 
+                    React.createElement("img", {className: this.props.img ===undefined ? 'hide' : '', src: this.props.img})
+                ), 
+                React.createElement("div", {className: "large-9 columns"}, 
+                  this.props.title
+                )
+              )
+              )
+            )
           );
       }
 
     });
 
-    var PostList = React.createClass({
+    var PostList = React.createClass({displayName: "PostList",
       getInitialState:function(){
         return{
             cursor: 0
@@ -84,38 +85,38 @@
         var is_selected = outer_this.state.cursor == count ? "is_selected" : "";
         count+=1;
         return (
-          <Post title={post.title} url={post.url} img={post.image} keywords={post.keywords} is_selected={is_selected}/>
+          React.createElement(Post, {title: post.title, url: post.url, img: post.image, keywords: post.keywords, is_selected: is_selected})
         );
       });
 
         return(
-          <ul id="search-items">
-            {posts}
-          </ul>
+          React.createElement("ul", {id: "search-items"}, 
+            posts
+          )
         );
       }
 
     });
 
-    var SearchBar = React.createClass({
+    var SearchBar = React.createClass({displayName: "SearchBar",
         update_search:function(){
             var query_text=this.refs.search_input.getDOMNode().value;
             this.props.update_searchbox(query_text);
         },
 
         render:function(){
-            return <input type="text" 
-                    id="search-bar" 
-                    ref="search_input" 
-                    placeholder="Search for a post, title, or keyword" 
-                    required="required" 
-                    value={this.props.query_text}
-                    onChange={this.update_search}/>
+            return React.createElement("input", {type: "text", 
+                    id: "search-bar", 
+                    ref: "search_input", 
+                    placeholder: "Search for a post, title, or keyword", 
+                    required: "required", 
+                    value: this.props.query, 
+                    onChange: this.update_search})
         }
 
     });
 
-    var SearchBox = React.createClass({
+    var SearchBox = React.createClass({displayName: "SearchBox",
 
       getInitialState:function(){
         return{
@@ -182,10 +183,10 @@
 
       render: function(){
         return(
-          <div id="search-box">
-            <SearchBar update_searchbox={this.update_state} query_text={this.state.query_text} />
-            <PostList data={this.state.filtered_data} />
-          </div>
+          React.createElement("div", {id: "search-box"}, 
+            React.createElement(SearchBar, {update_searchbox: this.update_state, query_text: this.state.query_text}), 
+            React.createElement(PostList, {data: this.state.filtered_data})
+          )
         );
       }
 
@@ -193,7 +194,7 @@
   
     
     React.render(
-      <SearchBox data={blog_data}/>,
+      React.createElement(SearchBox, {data: blog_data}),
       document.getElementById('search-container')
     );
 
--- assets/js/application.js
+++ assets/js/application.js
@@ -1,10 +1,40 @@
-  console.log('require.js app');
+//For production use only; console.log was causing weird errors when dev tools were not open in chrome
 
-  require.config({
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+console.log('require.js app');
+
+require.config({
     "baseUrl": "/assets/js",
     "paths": {
         "jquery": "jquery",
         "underscore": "underscore",
+        "fitvids": "jquery.fitvids",
+        "index": "index",
+        "readtime": "readingTime.min",
+        "react": "react",
+        "searchdata": "searchdata",
+        "searchbar": "app/searchbar"
     },
     "shim": {
         "jquery": {
@@ -12,12 +42,45 @@
         },
         "underscore": {
             "exports": "_"
+        },
+        "fitvids": {
+            "deps": ["jquery"]
+        },
+        "index": {
+            "deps": ["jquery", "readtime", "fitvids"]
+        },
+        "readtime": {
+            "deps": ["jquery"]
+        },
+        "react": {
+            "deps": ["jquery"],
+            "exports": "_React"
+        },
+        "searchbar": {
+            "deps": ["jquery", "underscore", "react", "searchdata"]
         }
     }
-  });
-
-require(["jquery", "underscore"], function($, _) {
-    $(function() {
-        console.log('base application loaded');
-    });
 });
+
+require(["jquery", "underscore", "react"], function($, _, _React) {
+    //GLOBAL REACT VARIABLE; needed for the below react components
+    React = _React; 
+
+    //REACT COMPONENTS;
+    switch ($('body').attr('class')){
+        case 'default':
+            require(["searchdata", "searchbar"], function() {
+                console.log('searchbar loaded');
+            });
+            break;
+        default:
+            break;
+    }
+
+    //OTHER JS
+    require(["fitvids", "index", "readtime"], function() {
+        console.log('base js loaded');
+    });
+
+});
+
